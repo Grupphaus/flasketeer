fee29b19605d3f1cf95438b695dd039ed4b3163e
Federico Pellegrini <federicop@lt-r13n-ba-120.ny.rga.com>
2020-03-11 12:26:24 -0300

    project structure, header navigation

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('/about/about.jinja', title="About")
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('/portfolio/portfolio.jinja', title="Portfolio")
+
+@app.route('/blog')
+def blog():
+    return render_template('/blog/blog.jinja', title="Blog")
+
+@app.route('/contact')
+def contact():
+    return render_template('/contact/contact.jinja', title="Contact")
